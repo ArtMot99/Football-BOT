fda497290de970a17a38a7fc7eaf48448d54d80b
Artem Motrunich <motrunich2013@gmail.com>
2024-12-07 19:15:07 +0100

    Add poll to kb

--- app/keyboards.py
+++ app/keyboards.py
@@ -3,7 +3,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 main = ReplyKeyboardMarkup(keyboard=[
     [
         KeyboardButton(text="Запланировать тренировку"),
-        KeyboardButton(text="Послать QR и посчитать оплату"),
+        KeyboardButton(text="Начать опрос"),
     ],
+    [KeyboardButton(text="Послать QR и посчитать оплату")],
     [KeyboardButton(text="Отменить тренировку")]
 ])
