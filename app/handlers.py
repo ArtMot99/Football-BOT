c0ec7e36d0a2dabb1775e67b0e38091103c3c60e
Artem Motrunich <motrunich2013@gmail.com>
2024-12-07 11:09:49 +0100

    Add action "writing message" for BOT

--- app/handlers.py
+++ app/handlers.py
@@ -1,15 +1,28 @@
+from asyncio import sleep
+
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
+from aiogram.enums import ChatAction
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.TYPING
+    )
+    await sleep(1)
     await message.answer(text="Привет!")
 
 
 @router.message()
 async def echo(message: Message):
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.TYPING
+    )
+    await sleep(1)
     await message.answer(text="Я не знаю эту команду 🤷🏻‍♂️")
